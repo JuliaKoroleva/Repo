ad3d590f2210e85f3de29f5d2a11eb51f438c6b5
Julia <juliakoroleva@inbox.ru>
2016-01-11 17:41:56 +0300

    AddedMigrations

---
+++ SourceControl/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceControl
+{
+    class Context: DbContext
+    {
+        public DbSet<Song> songs { get; set; }
+        public DbSet<Artist> artists { get; set; }
+    }
+}
