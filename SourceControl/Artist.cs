8827f22471df56f14978e7fe24ff6a11e35d2dc2
Igor <ivryazanov@edu.hse.ru>
2016-01-11 17:19:37 +0300

    ClassesAdded

---
+++ SourceControl/Artist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceControl
+{
+    class Artist
+    {
+        public string ArtistName { get; set; }
+        public List<Song> SongList { get; set; }
+
+        public Artist(string ArtistName, List<Song> SongList)
+        {
+            this.ArtistName = ArtistName;
+            this.SongList = SongList;
+        }
+    }
+}
---
+++ SourceControl/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceControl
+{
+    class Song
+    {
+        public string Name { get; set; }
+        public int Duration { get; set; }
+        public Artist Artist { get; set; }
+
+        public Song (string Name, int Duration, Artist Artist)
+        {
+            this.Duration = Duration;
+            this.Name = Name;
+            this.Artist = Artist;
+        }
+    }
+}
